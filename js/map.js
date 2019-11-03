26e17b7229ce9f6c4585eca77a9f129b70e852c8
YA <YA-androidapp@users.noreply.github.com>
2019-11-03 15:42:52 +0900

    Add showing distances

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@ function map_init() {
     var mpoint = [35.531388046719215, 139.69683229923248];
 
     var currentZoom = map.getZoom();
-    var newZoom = currentZoom ? currentZoom : 18;
+    var newZoom = currentZoom ? currentZoom : 14;
     map.setView(mpoint, newZoom);
     L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
